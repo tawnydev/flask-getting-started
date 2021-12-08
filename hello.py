b357bdd1688989ec6baa29a477491de67b4725b0
tawnydev <kevin.matrix@hotmail.fr>
2021-12-08 17:49:40 -0500

    quickstart Flask

---
+++ hello.py
@@ -0,0 +1,49 @@
+from flask import Flask
+from flask import url_for
+from flask import request
+from flask import render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return f"Identifiant: <br> Mot de passe:"
+    else:
+        return url_for('show_user_profile', username='John Doe')
+
+@app.route("/<name>")
+def hello(name):
+    return f"Hello, {escape(name)}!"
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'User {escape(username)} profile'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return f'Post {post_id}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return f'Subpath {escape(subpath)}'
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('show_user_profile', username='John Doe'))
+    url_for('static', filename='style.css')

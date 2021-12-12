046ffc86e73390eaba0ae74ca853e28c4610afb3
tawnydev <kevin.matrix@hotmail.fr>
2021-12-11 19:27:40 -0500

    upload a file in the hello page signed in

--- hello.py
+++ hello.py
@@ -2,8 +2,10 @@ from flask import Flask
 from flask import url_for
 from flask import request
 from flask import render_template
+from werkzeug.utils import secure_filename
 from markupsafe import escape
 from helper import valid_login, log_the_user_in
+import os
 
 app = Flask(__name__)
 
@@ -45,6 +47,17 @@ def show_post(post_id):
 def show_subpath(subpath):
     # show the subpath after /path/
     return f'Subpath {escape(subpath)}'
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['filename']
+        path=os.getcwd()
+        fileName= secure_filename(file.filename)
+        print("path = ", path, "/", fileName)
+        file.save(f"{path}/{fileName}")
+        return render_template('hello.html', name=request.form['user'], upload=1)
+    return render_template('hello.html', name=request.form['user'], upload=0)
 
 with app.test_request_context():
     print(url_for('index'))

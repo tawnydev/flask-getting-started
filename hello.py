8c4067504c5b460c845ef16c96f501930a088ec7
tawnydev <kevin.matrix@hotmail.fr>
2021-12-11 20:12:07 -0500

    improve file upload

--- hello.py
+++ hello.py
@@ -1,7 +1,5 @@
 from flask import Flask
-from flask import url_for
-from flask import request
-from flask import render_template
+from flask import url_for, request, render_template, flash
 from werkzeug.utils import secure_filename
 from markupsafe import escape
 from helper import valid_login, log_the_user_in
@@ -9,14 +7,23 @@ import os
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = os.getcwd()
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+#MAX 16Mo
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
+
 @app.route('/')
 def index():
     return render_template('content_exemple.html')
+
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
+
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -33,31 +40,49 @@ def login():
     # was GET or the credentials were invalid
     return render_template('login.html', text=searchword, error=error)
 
+
 @app.route('/user/<username>')
 def show_user_profile(username):
     # show the user profile for that user
     return f'User {escape(username)} profile'
+
 
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
     # show the post with the given id, the id is an integer
     return f'Post {post_id}'
 
+
 @app.route('/path/<path:subpath>')
 def show_subpath(subpath):
     # show the subpath after /path/
     return f'Subpath {escape(subpath)}'
 
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
+        # check if the post request has the file part
+        if 'filename' not in request.files:
+            print('No file part')
+            return render_template('hello.html', name=request.form['user'], upload=0)
         file = request.files['filename']
-        path=os.getcwd()
-        fileName= secure_filename(file.filename)
-        print("path = ", path, "/", fileName)
-        file.save(f"{path}/{fileName}")
-        return render_template('hello.html', name=request.form['user'], upload=1)
-    return render_template('hello.html', name=request.form['user'], upload=0)
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            print('No selected file')
+            return render_template('hello.html', name=request.form['user'], upload=0)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            print('file uploaded')
+            return render_template('hello.html', name=request.form['user'], upload=1)
+
 
 with app.test_request_context():
     print(url_for('index'))
